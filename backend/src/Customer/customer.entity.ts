ca9bdb09e4a838f50d3bf070fe730f572b8399bf
Siampc <tahasinahmedsiam20@gmail.com>
2025-08-10 13:07:02 +0600

    set

--- backend/src/Customer/customer.entity.ts
+++ backend/src/Customer/customer.entity.ts
@@ -1,3 +1,4 @@
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn } from 'typeorm';
 
 @Entity('customers')
 export class Customer {
--- backend/src/Products/product.entity.ts
+++ backend/src/Products/product.entity.ts
@@ -1,3 +1,4 @@
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn } from 'typeorm';
 
 @Entity('products')
 export class Product {
